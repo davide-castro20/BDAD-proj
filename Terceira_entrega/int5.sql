8436967d5c374b28855b486f57da10c42d48247c
Henrique <gamingwithapotato@gmail.com>
2020-05-22 16:12:47 +0100

    Added new Query (Popularity)

--- Terceira_entrega/int5.sql
+++ Terceira_entrega/int5.sql
@@ -2,6 +2,23 @@
 .headers	on
 .nullvalue	NULL
 
-select ID, content, idMainComment
-from Comment left outer join Replies
-on ID=idReply;
+SELECT Subs.IDchannel, AVG(Views.nViews+Views.userReaction+Subs.nSubs) AS popularityScore
+FROM
+    (
+    SELECT Channel.ID, SUM(IFNULL(videoViews.nViews,0)) AS nViews, SUM(IFNULL(videoViews.userReaction,0)) AS userReaction
+    FROM Channel LEFT OUTER JOIN
+        (
+        SELECT IDVideo, COUNT(IDuser) AS nViews, SUM(reaction) AS userReaction
+        FROM ViewVideo
+        GROUP BY (IDVideo)
+        ) AS videoViews , Video
+    WHERE(Video.ID = videoViews.IDvideo AND Video.IDchannel = Channel.ID)
+    GROUP BY (Channel.ID)
+    ) as Views INNER JOIN
+    (
+    SELECT IDchannel, COUNT(IDuser) AS nSubs
+    FROM Subscribes
+    GROUP BY (IDchannel)
+    ) as Subs ON (Subs.IDchannel = Views.ID)
+GROUP BY (Subs.IDchannel)
+ORDER BY popularityScore DESC;
